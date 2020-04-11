aff224d9cb21c79aed92b19bfe3642ee62b06940
Ex-Communicad0 <57829893+Ex-Communicad0@users.noreply.github.com>
2020-04-11 17:47:42 +0600

    Update app.js

--- themes/material/app.js
+++ themes/material/app.js
@@ -247,18 +247,7 @@ function file_video(path){
 	<video class="mdui-video-fluid mdui-center" preload controls>
 	  <source src="${url}" type="video/mp4">
 	</video>
-	<br>
-	<!-- 固定标签 -->
-	<!-- 固定标签 -->
-	<div class="mdui-textfield">
-	
-	  <label class="mdui-textfield-label">下载地址</label>
-	  <input class="mdui-textfield-input" type="text" value="${url}"/>
-	</div>
-	<div class="mdui-textfield">
-	  <label class="mdui-textfield-label">引用地址</label>
-	  <textarea class="mdui-textfield-input"><video><source src="${url}" type="video/mp4"></video></textarea>
-	</div>
+	<br>${playBtn}
 </div>
 	`;
 	$('#content').html(content);
